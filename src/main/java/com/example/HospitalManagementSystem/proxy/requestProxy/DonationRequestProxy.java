abf1c129626065d76334b08dc11e7878c088b725
AjayPrajapati <ajayprajapati06245@gmail.com>
2026-03-10 12:36:28 +0530

    Initial Commit

---
+++ src/main/java/com/example/HospitalManagementSystem/proxy/requestProxy/DonationRequestProxy.java
@@ -0,0 +1,15 @@
+package com.example.HospitalManagementSystem.proxy.requestProxy;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class DonationRequestProxy {
+    private Long donationId;
+    private String bloodGroup;
+}
